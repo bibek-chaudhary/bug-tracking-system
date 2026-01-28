d257b8798ffa051432e9d871ca8ac5a2ed03caea
Bibek Chaudhary <bibek4929@gmail.com>
2026-01-28 15:53:53 +0545

    feat(identity): setup identity db context and ef core

---
+++ backend/BugTracker/BugTracker.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Infrastructure.Persistence
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
