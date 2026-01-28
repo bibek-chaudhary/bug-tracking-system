5ada1fcc13de90de577f1bd66108f3ca589b5f5d
Bibek Chaudhary <bibek4929@gmail.com>
2026-01-29 00:32:02 +0545

    feat(bugs): allow users to view their reported bugs

---
+++ backend/BugTracker/BugTracker.Application/DTOs/Bugs/MyBugsResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.DTOs.Bugs
+{
+    public class MyBugsResponseDto
+    {
+        public Guid Id { set; get; }
+        public string Title { set; get; } = null!;
+        public BugSeverity Severity { set; get; }
+        public BugStatus Status { set; get; }
+        public DateTime CreatedAt { set; get; }
+    }
+}
