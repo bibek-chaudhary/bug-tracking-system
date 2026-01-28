bd90dd3d80be5256a51ca27e63005bd45eadbf3a
Bibek Chaudhary <bibek4929@gmail.com>
2026-01-29 00:15:06 +0545

    feat(bugs): allow users to report bugs with attachments

---
+++ backend/BugTracker/BugTracker.Api/Controllers/BugsController.cs
@@ -0,0 +1,41 @@
+﻿using BugTracker.Application.DTOs.Bugs;
+using BugTracker.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BugTracker.Api.Controllers
+{
+    [ApiController]
+    [Route("api/bugs")]
+    [Authorize(Roles = "User")]
+    public class BugsController : ControllerBase
+    {
+        private readonly IBugService _bugService;
+        public BugsController(IBugService bugService)
+        {
+            _bugService = bugService;
+        }
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> ReportBug([FromForm] CreateBugRequestDto request)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new UnauthorizedAccessException();
+
+            await _bugService.CreateBugAsync(request, userId);
+            return Ok("Bug reported successfully");
+        }
+
+        //[HttpPost]
+        //public IActionResult Test()
+        //{
+        //    return Ok("HIT");
+        //}
+
+        [HttpGet]
+        public IActionResult Ping() => Ok("API works");
+
+
+    }
+}
--- backend/BugTracker/BugTracker.Api/Program.cs
+++ backend/BugTracker/BugTracker.Api/Program.cs
@@ -13,22 +13,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+builder.Services.AddScoped<IBugService, BugService>();
+builder.Services.AddScoped<FileStorageService>();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
         ValidateAudience = true,
+        ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
         ValidAudience = builder.Configuration["Jwt:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
     };
 });
 
 builder.Services.AddAuthorization();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
