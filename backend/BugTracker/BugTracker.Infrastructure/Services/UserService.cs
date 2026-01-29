97ab5487bd9b31cb6bdc1c026276977d196ee8e2
Bibek Chaudhary <bibek4929@gmail.com>
2026-01-30 00:37:29 +0545

    feat(api): add endpoint to fetch developers for bug assignment

---
+++ backend/BugTracker/BugTracker.Infrastructure/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using BugTracker.Application.DTOs;
+using BugTracker.Application.Interfaces.Services;
+using BugTracker.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<List<DeveloperDto>> GetDevelopersAsync()
+        {
+            var developers = await _userManager.GetUsersInRoleAsync("Developer");
+
+            return developers.Select(d => new DeveloperDto
+            {
+                Id = d.Id,
+                Name = d.UserName!
+            }).ToList();
+        }
+    }
+
+}
